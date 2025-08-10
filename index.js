8c05249ac770f4ba6a7ad04fe2bf598f2ae118b6
CuriouzK0d3r <curiouz_k0d3r@protonmail.com>
2025-08-10 15:33:56 +0300

    electron package

---
+++ index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+/**
+ * Writers CLI - Main Entry Point
+ * 
+ * This file serves as the main entry point for the Writers CLI application.
+ * It delegates to the actual CLI implementation in bin/writers.js.
+ */
+
+// Import and run the main CLI application
+require('./bin/writers.js');
